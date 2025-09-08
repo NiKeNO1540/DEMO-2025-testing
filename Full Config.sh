bc609ab244539528a6f7076ecdcb27d30f0ecb6e
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-09-08 12:39:52 +0500

    Update Full Config.sh

--- Full Config.sh
+++ Full Config.sh
@@ -9,12 +9,20 @@ echo -e "BOOTPROTO=static\nTYPE=eth\nDISABLED=no\nCONFIG_IPV4=yes" > /etc/net/if
 sed -i "s/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/" /etc/net/sysctl.conf
 systemctl restart network
 
+# Раздача ключей
+
+ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa -q
+ssh-keyscan -H 172.16.4.4 >> ~/.ssh/known_hosts
+apt-get install sshpass -y
+sshpass -p 'admin' ssh-copy-id admin@172.16.4.4
+ssh-keyscan -H 172.16.5.5 >> ~/.ssh/known_hosts
+sshpass -p 'admin' ssh-copy-id admin@172.16.5.5
+
 # Настройка HQ-RTR-BR-RTR-Коммутация(Это очень мощный костыль, но рабочий, базируется на expect.)
 
-cd DEMO-2025-TESTING
+cd DEMO-2025-testing
 apt-get update
 apt-get install expect -y
-apt-get install sshpass -y
 systemctl enable --now sshd
-expect hq-rtr.exp
-expect br-rtr.exp
+expect hq-rtr-module-1.exp
+expect br-rtr-module-1.exp
