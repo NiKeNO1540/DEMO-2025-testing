1b41b5c4a69ca7205715d13900517fc99faccb88
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-09-11 15:42:05 +0500

    Update HQ-SRV-Launch.sh

--- HQ-SRV-Launch.sh
+++ HQ-SRV-Launch.sh
@@ -12,13 +12,12 @@ systemctl enable --now dnsmasq
 cat << EOF >> /etc/dnsmasq.conf
 no-resolv
 domain=au-team.irpo
-server=/au-team.irpo/
 server=8.8.8.8
 interface=*
 address=/hq-rtr.au-team.irpo/192.168.1.1
 ptr-record=1.1.168.192.in-addr.arpa,hq-rtr.au-team.irpo
-cname=moodle.au-team.irpo,hq-rtr.au-team.irpo
-cname=wiki.au-team.irpo,hq-rtr.au-team.irpo
+address=/web.au-team.irpo/172.16.1.1
+address=/docker.au-team.irpo/172.16.2.1
 address=/br-rtr.au-team.irpo/192.168.3.1
 address=/hq-srv.au-team.irpo/192.168.1.10
 ptr-record=10.1.168.192.in-addr.arpa,hq-srv.au-team.irpo
