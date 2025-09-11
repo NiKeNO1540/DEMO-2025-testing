bf80deca312742e87add88724faa8b94806be68a
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-09-11 14:54:23 +0500

    Update Full_Config.sh

--- Full_Config.sh
+++ Full_Config.sh
@@ -13,8 +13,8 @@ systemctl restart network
 # Настройка IPTABLES
 
 apt-get install iptables -y
-iptables -t nat -A POSTROUTING -o ens20 -s 172.16.4.0/28 -j MASQUERADE
-iptables -t nat -A POSTROUTING -o ens20 -s 172.16.5.0/28 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o ens20 -s 172.16.1.0/28 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o ens20 -s 172.16.2.0/28 -j MASQUERADE
 iptables-save > /etc/sysconfig/iptables
 systemctl enable –-now iptables
 
@@ -23,11 +23,11 @@ systemctl enable –-now iptables
 ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa -q
 ssh-keyscan -H 172.16.4.4 >> ~/.ssh/known_hosts
 apt-get install sshpass -y
-sshpass -p 'admin' ssh-copy-id admin@172.16.4.4
+sshpass -p 'admin' ssh-copy-id admin@172.16.1.4
 ssh-keyscan -H 172.16.5.5 >> ~/.ssh/known_hosts
-sshpass -p 'admin' ssh-copy-id admin@172.16.5.5
+sshpass -p 'admin' ssh-copy-id admin@172.16.2.5
 
-# Настройка HQ-RTR|BR-RTR-Коммутация(Если по простому, базируется на expect, очень зависимый на переменных)
+# Настройка HQ-RTR|BR-RTR-Коммутация(Если по простому, базируется на инструментарии expect, очень зависимый на переменных)
 
 cd DEMO-2025-testing
 apt-get update
@@ -35,3 +35,5 @@ apt-get install expect -y
 systemctl enable --now sshd
 expect hq-rtr-module-1.exp
 expect br-rtr-module-1.exp
+
+
