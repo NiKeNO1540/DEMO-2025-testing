2a2f88237cc4646ba9b2d3072101687e2c581daa
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-09-12 11:26:40 +0500

    Create samba-part-2.sh

---
+++ samba-part-2.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+wget https://raw.githubusercontent.com/sudo-project/sudo/main/docs/schema.ActiveDirectory
+dos2unix schema.ActiveDirectory
+sed -i 's/DC=X/DC=au-team,DC=irpo/g' schema.ActiveDirectory
+head -$(grep -B1 -n '^dn:$' schema.ActiveDirectory | head -1 | grep -oP '\d+') schema.ActiveDirectory > first.ldif
+tail +$(grep -B1 -n '^dn:$' schema.ActiveDirectory | head -1 | grep -oP '\d+') schema.ActiveDirectory | sed '/^-/d' > second.ldif
+ldbadd -H /var/lib/samba/private/sam.ldb first.ldif --option="dsdb:schema update allowed"=true
+ldbmodify -v -H /var/lib/samba/private/sam.ldb second.ldif --option="dsdb:schema update allowed"=true
+samba-tool ou add 'ou=sudoers'
+
+cat << EOF > sudoRole-object.ldif
+dn: CN=prava_hq,OU=sudoers,DC=test,DC=alt
+changetype: add
+objectClass: top
+objectClass: sudoRole
+cn: prava_hq
+name: prava_hq
+sudoUser: %hq
+sudoHost: ALL
+sudoCommand: /bin/grep
+sudoCommand: /bin/cat
+sudoCommand: /usr/bin/id
+sudoOption: !authenticate
+EOF
+ldbadd -H /var/lib/samba/private/sam.ldb sudoRole-object.ldif
