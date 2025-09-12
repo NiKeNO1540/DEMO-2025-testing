33b188b28919b1cb73cde47d16094172e4b7cf99
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-09-12 11:34:25 +0500

    Update Full_Config.sh

--- Full_Config.sh
+++ Full_Config.sh
@@ -30,6 +30,7 @@ ssh-keyscan -p 2026 172.16.1.4 >> ~/.ssh/known_hosts
 ssh-keyscan -p 2026 172.16.2.5 >> ~/.ssh/known_hosts
 sshpass -p 'toor' ssh-copy-id -p 2026 root@172.16.2.5
 sshpass -p 'toor' ssh-copy-id -p 2026 root@172.16.1.4
+sshpass -p 'toor' ssh-copy-id -p 2222 root@172.16.1.4
 
 # Настройка HQ-RTR|BR-RTR-Коммутация(Если по простому, базируется на инструментарии expect, очень зависимый на переменных)
 
@@ -40,4 +41,45 @@ systemctl enable --now sshd
 expect hq-rtr-module-1.exp
 expect br-rtr-module-1.exp
 
-ssh -p 2026 root@172.1.4 "bash -s" < HQ-SRV-Launch.sh
+ssh -p 2026 root@172.16.1.4 "bash -s" < HQ-SRV-Launch.sh
+
+ssh -p 2026 root@172.16.2.5 "bash -s" < samba-part-1.sh
+
+cat << EOF | ssh -p 2222 root@172.16.1.4
+echo nameserver 8.8.8.8 >> /etc/resolv.conf && apt-get update && apt-get install bind-utils -y
+system-auth write ad AU-TEAM.IRPO cli AU-TEAM 'administrator' 'P@ssw0rd'
+reboot
+EOF
+
+ssh -p 2026 root@172.16.2.5 "bash -s" < samba-part-2.sh
+
+cat << EOF | ssh -p 2222 root@172.16.1.4
+apt-get install sudo libsss_sudo -y
+control sudo public
+
+sed -i '19 a\
+sudo_provider = ad' /etc/sssd/sssd.conf
+sed -i 's/services = nss, pam,/services = nss, pam, sudo' /etc/sssd/sssd.conf
+sed -i '28 a\
+sudoers: files sss' /etc/nsswitch.conf
+
+reboot
+EOF
+
+sleep 15
+
+cat << EOF | ssh -p 2222 root@172.16.1.4
+rm -rf /var/lib/sss/db/*
+sss_cache -E
+systemctl restart sssd
+
+sudo -l -U hquser1
+reboot
+EOF
+
+sleep 15
+
+sshpass -p 'toor' ssh-copy-id -p 2222 hquser1@172.16.1.4
+cat << EOF | ssh -p 2222 hquser1@172.16.1.4
+sudo cat /etc/passwd | sudo grep root && sudo id root
+EOF
