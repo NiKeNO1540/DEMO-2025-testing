0a0bee99d41bbf5f922a9d6d439d1490a2b25236
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-09-12 12:32:51 +0500

    Update Full_Config.sh

--- Full_Config.sh
+++ Full_Config.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Настройка ISP
+echo "Настройка ISP"
 
 mkdir /etc/net/ifaces/ens21
 mkdir /etc/net/ifaces/ens22
@@ -10,7 +10,7 @@ echo 172.16.1.1/28 > /etc/net/ifaces/ens21/ipv4address
 echo 172.16.2.1/28 > /etc/net/ifaces/ens22/ipv4address
 systemctl restart network
 
-# Настройка IPTABLES
+echo "Настройка IPTABLES"
 
 apt-get install iptables -y
 iptables -t nat -A POSTROUTING -o ens20 -s 172.16.1.0/28 -j MASQUERADE
@@ -18,7 +18,7 @@ iptables -t nat -A POSTROUTING -o ens20 -s 172.16.2.0/28 -j MASQUERADE
 iptables-save > /etc/sysconfig/iptables
 systemctl enable –-now iptables
 
-# Раздача ключей
+echo "Раздача ключей"
 
 ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa -q
 ssh-keyscan -H 172.16.1.4 >> ~/.ssh/known_hosts
@@ -27,7 +27,7 @@ sshpass -p 'admin' ssh-copy-id admin@172.16.1.4
 ssh-keyscan -H 172.16.2.5 >> ~/.ssh/known_hosts
 sshpass -p 'admin' ssh-copy-id admin@172.16.2.5
 
-# Настройка HQ-RTR|BR-RTR-Коммутация(Если по простому, базируется на инструментарии expect, очень зависимый на переменных)
+echo "Настройка HQ-RTR|BR-RTR-Коммутация(Если по простому, базируется на инструментарии expect, очень зависимый на переменных)"
 
 cd DEMO-2025-testing
 apt-get update
@@ -36,14 +36,18 @@ systemctl enable --now sshd
 expect hq-rtr-module-1.exp
 expect br-rtr-module-1.exp
 
+echo "Окончательная раздача ключей"
+
 ssh-keyscan -p 2026 172.16.1.4 >> ~/.ssh/known_hosts
 ssh-keyscan -p 2026 172.16.2.5 >> ~/.ssh/known_hosts
 sshpass -p 'toor' ssh-copy-id -p 2026 root@172.16.2.5
 sshpass -p 'toor' ssh-copy-id -p 2026 root@172.16.1.4
 sshpass -p 'toor' ssh-copy-id -p 2222 root@172.16.1.4
 
-ssh -p 2026 root@172.16.1.4 "bash -s" < HQ-SRV-Launch.sh
+echo "Настройка DNS"
 
+ssh -p 2026 root@172.16.1.4 "bash -s" < HQ-SRV-Launch.sh
+echo "Настройка Samba"
 ssh -p 2026 root@172.16.2.5 "bash -s" < samba-part-1.sh
 
 cat << EOF | ssh -p 2222 root@172.16.1.4
