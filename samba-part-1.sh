6dccff5c4dd89cae6b2ca5f852dab5fd9001acbd
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-09-12 11:25:37 +0500

    Create samba-part-1.sh

---
+++ samba-part-1.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#! /bin/bash
+
+# Установка утилит
+apt-get install wget dos2unix task-samba-dc -y
+
+# Установка samba
+echo 192.168.1.10 >> /etc/resolv.conf
+echo 192.168.3.10 br-srv.au-team.irpo >> /etc/hosts
+# Для HQ-SRV: echo server=/au-team.irpo/192.168.3.10 >> /etc/dnsmasq.conf
+rm -rf /etc/samba/smb.conf
+samba-tool domain provision --realm=AU-TEAM.IRPO --domain=AU-TEAM --adminpass=P@ssw0rd --dns-backend=SAMBA_INTERNAL --server-role=dc --option='dns forwarder=192.168.1.10'
+mv -f /var/lib/samba/private/krb5.conf /etc/krb5.conf
+systemctl enable --now samba.service
+samba-tool user add hquser1 P@ssw0rd
+samba-tool user add hquser2 P@ssw0rd
+samba-tool user add hquser3 P@ssw0rd
+samba-tool user add hquser4 P@ssw0rd
+samba-tool user add hquser5 P@ssw0rd
+samba-tool group add hq
+samba-tool group addmembers hq hquser1, hquser2, hquser3, hquser4, hquser5
