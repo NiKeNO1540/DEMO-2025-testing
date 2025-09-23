c962712dddb11eaccf45dfe974b1f8080964c4aa
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-09-23 12:40:17 +0500

    Update cli-sssd-part1.sh

--- cli-sssd-part1.sh
+++ cli-sssd-part1.sh
@@ -4,7 +4,7 @@ apt-get install sudo libsss_sudo -y
 control sudo public
 sed -i '19 a\
 sudo_provider = ad' /etc/sssd/sssd.conf
-sed -i 's/services = nss, pam,/services = nss, pam, sudo' /etc/sssd/sssd.conf
+sed -i 's/services = nss, pam/services = nss, pam, sudo/' /etc/sssd/sssd.conf
 sed -i '28 a\
 sudoers: files sss' /etc/nsswitch.conf
 
