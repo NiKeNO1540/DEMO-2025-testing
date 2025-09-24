6f39dd4997f774439dade6677c19224a183eed70
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-09-24 12:37:37 +0500

    Create Raid-HQ-SRV.sh

---
+++ Raid-HQ-SRV.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+mdadm --create /dev/md0 --level=0 --raid-devices=2 /dev/sd[b-c]
+mdadm --detail -scan --verbose > /etc/mdadm.conf
+apt-get update && apt-get install fdisk -y
+echo -e "n\np\n1\n2048\n4186111\nw" | fdisk /dev/md0
+
+mkfs.ext4 /dev/md0p1
+cat << EOF >> /etc/fstab
+/dev/md0p1  /raid  ext4  defaults  0  0
+EOF
+
+mkdir /raid
+mount -a
+
+apt-get install nfs-server -y
+mkdir /raid/nfs
+chown 99:99 /raid/nfs
+chmod 777 /raid/nfs
+
+cat << EOF >> /etc/exports
+/raid/nfs  192.168.2.0/28(rw,sync,no_subtree_check)
+EOF
+exportfs -a
+exportfs -v
+systemctl enable nfs
+systemctl restart nfs
