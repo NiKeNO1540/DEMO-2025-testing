791a56e0296840e36fd251684bfa9546baec0371
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-09-24 15:11:54 +0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -7,4 +7,6 @@ mount -o loop /dev/sr0
 docker load -i /media/ALTLinux/docker/site_latest.tar
 docker load -i /media/ALTLinux/docker/mariadb_latest.tar
 
-docker compose -f site.yml up -d && sleep 5 && docker exec -it db mysql -u root -pPassw0rd -e "CREATE DATABASE IF NOT EXISTS testdb; CREATE USER 'test'@'%' IDENTIFIED BY 'Passw0rd'; GRANT ALL PRIVILEGES ON testdb.* TO 'test'@'%'; FLUSH PRIVILEGES;"
+docker compose -f site.yml up -d 
+sleep 5 
+docker exec -it db mysql -u root -pPassw0rd -e "CREATE DATABASE IF NOT EXISTS testdb; CREATE USER 'test'@'%' IDENTIFIED BY 'Passw0rd'; GRANT ALL PRIVILEGES ON testdb.* TO 'test'@'%'; FLUSH PRIVILEGES;"
