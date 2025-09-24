7a862b533875dfce6c127560d74efdf12e292449
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-09-24 15:20:17 +0500

    Update site.sh

--- site.sh
+++ site.sh
@@ -44,8 +44,8 @@ chmod -R 755 /var/www/html
 # Исправляем учетные данные для подключения
 sed -i "s/\$servername = .*;/\$servername = 'localhost';/" /var/www/html/index.php
 sed -i "s/\$dbname = .*;/\$dbname = 'webdb';/" /var/www/html/index.php
-sed -i "s/\$password = .*;/\$password = 'webc';/" /var/www/html/index.php
-sed -i "s/\$username = .*;/\$username = 'P@ssw0rd';/" /var/www/html/index.php
+sed -i "s/\$password = .*;/\$password = 'P@ssw0rd';/" /var/www/html/index.php
+sed -i "s/\$username = .*;/\$username = 'webc';/" /var/www/html/index.php
 
 sed -i 's/DirectoryIndex index.html index.cgi index.pl index.php index.xhtml index.htm/DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm/' /etc/httpd2/conf/mods-enabled/dir.conf
 rm -rf /var/www/html/index.html
