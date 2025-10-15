00282a43ff7b3319d4df36a3592456a0b42fed33
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-10-15 10:53:37 +0500

    Update cli-sssd-part1.sh

--- cli-sssd-part1.sh
+++ cli-sssd-part1.sh
@@ -10,5 +10,3 @@ sudo_provider = ad' /etc/sssd/sssd.conf
 sed -i 's/services = nss, pam/services = nss, pam, sudo/' /etc/sssd/sssd.conf
 sed -i '28 a\
 sudoers: files sss' /etc/nsswitch.conf
-
-reboot
