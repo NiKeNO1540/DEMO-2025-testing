194a29cd3ee7f5f51dddb5c9c2d8d6d893e18811
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-11-17 13:55:31 +0500

    Update Full_Config_Progression_AIO.sh

--- Full_Config_Progression_AIO.sh
+++ Full_Config_Progression_AIO.sh
@@ -838,8 +838,25 @@ EOF
     fi
 
 
-    chmod +x ssh_restrict.sh
-    ./ssh_restrict.sh
+    cat << EOF | sshpass -p 'toor' ssh -p 2026 root@172.16.1.4
+    echo -e 'AllowUsers sshuser\nMaxAuthTries 2\nBanner /root/banner' >> /etc/openssh/sshd_config
+    echo 'Authorized Access Only' > /root/banner
+    sed -i 's/# WHEEL_USERS ALL=(ALL:ALL) NOPASSWD: ALL/WHEEL_USERS ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+    if ! id sshuser | grep wheel; then
+    gpasswd -a 'sshuser' wheel
+    fi
+    systemctl restart sshd
+    EOF
+
+    cat << EOF | sshpass -p 'toor' ssh -p 2026 root@172.16.2.5
+    echo -e 'AllowUsers sshuser\nMaxAuthTries 2\nBanner /root/banner' >> /etc/openssh/sshd_config
+    echo 'Authorized Access Only' > /root/banner
+    sed -i 's/# WHEEL_USERS ALL=(ALL:ALL) NOPASSWD: ALL/WHEEL_USERS ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+    if ! id sshuser | grep wheel; then
+    gpasswd -a 'sshuser' wheel
+    fi
+    systemctl restart sshd
+    EOF
     if [ $? -eq 0 ]; then
         log_message "Переделан SSH"
     else
